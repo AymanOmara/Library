8bd12105118629111dd821bd9b19d2455136108a
AymanOmara <75618602+AymanOmara@users.noreply.github.com>
2022-03-04 20:41:27 +0200

    finish welcome screen and craete Majors Screen

---
+++ app/src/main/java/com/example/liberary/MajorsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.liberary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MajorsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_majors)
+    }
+}
--- app/src/main/java/com/example/liberary/WelcomeActivity.kt
+++ app/src/main/java/com/example/liberary/WelcomeActivity.kt
@@ -1,11 +1,41 @@
 package com.example.liberary
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class WelcomeActivity : AppCompatActivity() {
+    private var businessEnglishBtn : Button? = null
+    private var businessArabicBtn : Button? = null
+    private var myIntent : Intent? = null
+    private var majors:ArrayList<String>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+        myIntent = Intent(this,MajorsActivity::class.java)
+        majors = ArrayList()
+        supportActionBar?.hide()
+        findView()
+        setUpOnClickAction()
+
+    }
+    fun findView(){
+        businessEnglishBtn = findViewById(R.id.businessEnglishbtn)
+        businessArabicBtn = findViewById(R.id.businessArabicbtn)
+    }
+    fun setUpOnClickAction() {
+        businessEnglishBtn?.setOnClickListener {
+
+            majors = arrayListOf("Ayman English","Ayman English","Ayman English","Ayman English")
+            myIntent?.putStringArrayListExtra("key",majors)
+            startActivity(myIntent)
+        }
+        businessArabicBtn?.setOnClickListener {
+            majors = arrayListOf("Ayman Arabic","Ayman Arabic","Ayman Arabic","Ayman Arabic")
+            myIntent?.putStringArrayListExtra("key",majors)
+            startActivity(myIntent)
+        }
     }
 }
