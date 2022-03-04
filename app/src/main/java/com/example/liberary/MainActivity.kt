d634e63eeeddb7a9858d80dc85180d26911e43cf
AymanOmara <75618602+AymanOmara@users.noreply.github.com>
2022-03-04 19:59:09 +0200

    finish launch screen and create welcome screen activity only

--- app/src/main/java/com/example/liberary/MainActivity.kt
+++ app/src/main/java/com/example/liberary/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.liberary
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+
 
 class MainActivity : AppCompatActivity() {
+    private var myIntent : Intent? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+        myIntent = Intent(this,WelcomeActivity::class.java)
+        Handler().postDelayed({
+            startActivity(myIntent)
+            finish()
+        },5000)
+
+
+
+
+
+
     }
+
 }
