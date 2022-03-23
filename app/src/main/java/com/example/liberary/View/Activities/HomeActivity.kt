04ed4581db73f8ab283ebe624f0c5e739701d075
AymanOmara <75618602+AymanOmara@users.noreply.github.com>
2022-03-23 14:03:18 +0200

    add switch control listner

--- app/src/main/java/com/example/liberary/View/Activities/HomeActivity.kt
+++ app/src/main/java/com/example/liberary/View/Activities/HomeActivity.kt
@@ -1,25 +1,68 @@
 package com.example.liberary.View.Activities
 
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SwitchCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import com.etebarian.meowbottomnavigation.MeowBottomNavigation
 import com.example.liberary.Course
+import com.example.liberary.R
 import com.example.liberary.View.Fragment.FavoriteFragment
 import com.example.liberary.View.Fragment.HomeFragment
-import com.example.liberary.R
 import com.example.liberary.View.Fragment.RecentOpenFragment
 import com.example.liberary.constants.Constants
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_home.*
-import java.util.*
+
+
 
 class HomeActivity : AppCompatActivity() {
+    var courses: ArrayList<Course>? = null
+    val bundle = Bundle()
+    lateinit var toggle :ActionBarDrawerToggle
+    lateinit var switch: SwitchCompat
     override fun onCreate(savedInstanceState: Bundle?) {
-        var courses: ArrayList<Course>? = null
-        val bundle = Bundle()
+
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        val menu: Menu = navigationView.menu
+        val drawerLayout:DrawerLayout = findViewById(R.id.navigationDrawer)
+        val navigationView:NavigationView = findViewById(R.id.navigationView)
+        toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val item =  menu.findItem(R.id.app_bar_switch)
+        val action = item?.actionView
+
+        action!!.findViewById<SwitchCompat>(R.id.switchDarkModeState)!!.setOnCheckedChangeListener { compoundButton, b ->
+            if (b){
+                Log.d("done","true")
+            }else{
+                Log.d("done","false")
+            }
+        }
+        navigationView.setNavigationItemSelectedListener {
+
+            when(it.itemId){
+                R.id.changeLanguage -> {Toast.makeText(applicationContext,"change language ",Toast.LENGTH_LONG).show()}
+                R.id.switchDarkModeState ->{
+
+                }
+            }
+
+
+            true
+        }
 
         bottomNavigation.show(0)
         bottomNavigation.add(MeowBottomNavigation.Model(0, R.drawable.homeicon))
@@ -57,6 +100,15 @@ class HomeActivity : AppCompatActivity() {
 
 
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            return  true
+        }
+        return super.onOptionsItemSelected(item)
+
+    }
+
     private fun replaceFragment(fragment: Fragment){
         val fragmentManager = supportFragmentManager
         val fragmentTransAction = fragmentManager.beginTransaction()
