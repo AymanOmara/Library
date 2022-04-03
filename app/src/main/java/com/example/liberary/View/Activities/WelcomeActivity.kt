9e83fcd5e4d22f9ef5125dc6f0a8aeaed05025e3
AymanOmara <75618602+AymanOmara@users.noreply.github.com>
2022-04-03 20:01:31 +0200

    add enhancement at the meating

--- app/src/main/java/com/example/liberary/View/Activities/WelcomeActivity.kt
+++ app/src/main/java/com/example/liberary/View/Activities/WelcomeActivity.kt
@@ -39,7 +39,7 @@ class WelcomeActivity : AppCompatActivity() {
 
             startActivity(myIntent)
         }
-        businessArabicBtn?.setOnClickListener {
+        businessArabicBtn.setOnClickListener {
 
             //startActivity(myIntent)
         }
