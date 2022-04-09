d32cf9934eec563329584507a511d813d2adc880
AymanOmara <75618602+AymanOmara@users.noreply.github.com>
2022-04-10 00:29:14 +0200

    start new download manager method

--- app/src/main/java/com/example/liberary/View/Activities/DetailsActivity.kt
+++ app/src/main/java/com/example/liberary/View/Activities/DetailsActivity.kt
@@ -2,13 +2,14 @@ package com.example.liberary.View.Activities
 
 
 import android.app.DownloadManager
+import android.content.Context
 import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
 import android.util.Log
 import android.webkit.URLUtil
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
@@ -32,25 +33,38 @@ class DetailsActivity : AppCompatActivity() {
     private lateinit var refrences: TextView
     private lateinit var manager: DownloadManager
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
         findViewByid()
         course = intent.getSerializableExtra(Constants.details) as Course
         bindDataToView()
-        manager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+        //manager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
         refrences.setOnClickListener {
             val refrence = refrences.text.toString()
 
             if (URLUtil.isValidUrl(refrence)) {
-                val request = DownloadManager.Request(Uri.parse(refrence))
+                manager = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+                val uri =
+                    Uri.parse("https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf")
+                val request = DownloadManager.Request(uri)
+                    .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE)
+                //request.
+                manager.enqueue(request)
+
+                Toast.makeText(this,"start loading",Toast.LENGTH_LONG).show()
+
+                    //.uri("content://com.example.app/documents/example.pdf")
+
+                /*val request = DownloadManager.Request(Uri.parse(refrence))
                      .setDestinationInExternalPublicDir(
                       Environment.DIRECTORY_DOWNLOADS, "${course.courseName+" "+course.courseCode}.pdf"
                     )
                     .setDestinationInExternalFilesDir(this,Environment.DIRECTORY_DOWNLOADS,"no")
                     .setAllowedOverRoaming(true)
                     .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE)
-                    manager.enqueue(request)
+                    manager.enqueue(request)*/
             } else {
                 showAlert(
                     resources.getString(R.string.Error),
@@ -88,8 +102,6 @@ class DetailsActivity : AppCompatActivity() {
         courseIntroduction.text = course.courseDescription
         coursePrerequest.text = course.preRequest
         refrences.text = course.refreces
-        Log.d("refrences inside course obj", course.refreces)
-        Log.d("refrences","${refrences.text}")
     }
 
     private fun addCourse(course: Course) {
