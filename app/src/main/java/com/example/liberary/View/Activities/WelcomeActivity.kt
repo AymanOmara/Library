76b83131262763edb84943f47d15d767dabfa5fd
20106 <magdyhadeer042@gmail.com>
2022-04-11 17:40:48 +0200

    add courses

--- app/src/main/java/com/example/liberary/View/Activities/WelcomeActivity.kt
+++ app/src/main/java/com/example/liberary/View/Activities/WelcomeActivity.kt
@@ -31,11 +31,31 @@ class WelcomeActivity : AppCompatActivity() {
     }
     fun setUpOnClickAction() {
         businessEnglishBtn.setOnClickListener {
-            val infromationSystemCourses:ArrayList<Course> =  arrayListOf(Course("machine learning", "info 401", "hello from description", "4", "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf","pre"),
-                Course("stat401","stat401","hello from my descroption","my refrence","https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"," 401"),Course("information 404","info404","hello from dec","1","https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf","401 , 406 ,407"))
+            val infromationSystemCourses:ArrayList<Course> =  arrayListOf(Course("Principles of Mathematics ", "MATH 101", "hello from description", "1", "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf","pre"),
+                Course("Financial Mathematics","MATH 201","hello from my descroption","2","https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"," 401"),
+                Course("Descriptive Statistics","STS 201","hello from dec","2","https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf","401 , 406 ,407"),
+                Course("Applied Statistics","STS 301","hello from my descroption","3","https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"," 401"),
+                Course("Advanced Statistics","STS 401","hello from my descroption","4","https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"," 401"),
+                Course("EMPTY FOR NOW","MATH 401","hello from my descroption","4","https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"," 401"),
+                Course("Introduction of Computer","COMP 102","hello from my descroption","1","https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf","COMP 101"),
+                Course("Principles of Information Systems","INF 201","hello from my descroption","2","https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf","COMP 102"),
+                Course("Principles of Programs Planning","INF 301","hello from my descroption","3","https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"," INF 201"),
+                Course("Basic of Systems Analysis","INF 302","hello from my descroption","3","https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf","INF 201"),
+                Course("Data Base","INF 303","hello from my descroption","3","https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf","INF 201"),
+                Course("System Design","INF 304","hello from my descroption","3","https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf","INF 302"),
+                Course("Experts Systems and Decision Support System","INF 306","hello from my descroption","3","https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf","INF 303") ,
+                Course("Machine Learning","INF 401","hello from my descroption","4","https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"," 401"),
+                Course("Data Communication Networks","INF 403","hello from my descroption","4","https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"," 401"),
+                Course("Systems Analysis & Design","INF 404","hello from my descroption","4","https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"," 401"),
+                Course("DataBases System Problems","INF 405","hello from my descroption","4","https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"," 401"),
+                Course("Medical Information Systems","INF 406","hello from my descroption","4","https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"," 401"),
+                Course("Advanced Information System","INF 407","hello from my descroption","4","https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"," 401"),
+            )
             val accountingCourses:ArrayList<Course> = arrayListOf(Course("accounting 404","account","hello","4","hjfgs","5256"))
+            val financeCourses:ArrayList<Course> = arrayListOf(Course("finance 404","404","jdhdj","4","https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf","fin405"))
             majors = arrayListOf(Major("Information systems", infromationSystemCourses),
-                Major("accounting",accountingCourses))
+                Major("accounting",accountingCourses), Major("finance",financeCourses)
+            )
             myIntent.putExtra(Constants.major,majors)
 
 
