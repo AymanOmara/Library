57c8df611b4448877be22462dc581184f1664087
AymanOmara <75618602+AymanOmara@users.noreply.github.com>
2022-04-19 22:40:34 +0200

    handle issue when no connection

--- app/src/main/java/com/example/liberary/View/Activities/PDFActivity.kt
+++ app/src/main/java/com/example/liberary/View/Activities/PDFActivity.kt
@@ -2,6 +2,8 @@ package com.example.liberary.View.Activities
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.liberary.R
 import com.example.liberary.constants.Constants
@@ -13,8 +15,8 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 class PDFActivity : AppCompatActivity(), Connector {
-    lateinit  var pdfView: PDFView
-    lateinit var pdfurl:String
+    private lateinit  var pdfView: PDFView
+    private lateinit var pdfurl:String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pdfactivity)
@@ -26,8 +28,8 @@ class PDFActivity : AppCompatActivity(), Connector {
 
     }
     class RetrivePDFfromUrl(private var pdfurl:String): AsyncTask<String, Void, InputStream>() {
-        var delagate:Connector? = null
-        var inputStream: InputStream? = null
+        lateinit var delagate:Connector
+        private  var inputStream: InputStream? = null
         override fun doInBackground(vararg p0: String?): InputStream? {
 
             try {
@@ -36,30 +38,38 @@ class PDFActivity : AppCompatActivity(), Connector {
                 // creating our connection.
                 val urlConnection: HttpURLConnection = url.openConnection() as HttpURLConnection
                 if (urlConnection.responseCode == 200) {
+
                     // response is success.
                     // we are getting input stream from url
                     // and storing it in our variable.
-                    inputStream = BufferedInputStream(urlConnection.getInputStream())
+                    inputStream = BufferedInputStream(urlConnection.inputStream)
                 }
+                Log.d("error message",urlConnection.responseMessage)
+                //Toast.makeText(this@PDFActivity,urlConnection.responseMessage,Toast.LENGTH_LONG).show()
+                //
+
             } catch (e: IOException) {
                 // this is the method
                 // to handle errors.
                 e.printStackTrace()
                 return null
             }
-            return inputStream!!
+            return inputStream
         }
 
         override fun onPostExecute(result: InputStream?) {
             super.onPostExecute(result)
-            delagate?.returnInputStream(inputStream)
+            delagate.returnInputStream(inputStream)
         }
 
 
     }
 
     override fun returnInputStream(result: InputStream?) {
-        pdfView.fromStream(result).load()
+        if(result != null){
+            pdfView.fromStream(result).load()
+        }
+
     }
 
 }
